2380271f7f190a5f4c8ce2efff4e1ab7c0db8330
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2025-01-01 16:20:41 +0530

    Hotels and Filter UI is updated

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,3 +1,97 @@
+// "use client";
+
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// const Filters = ({
+//   price,
+//   setPrice,
+//   handlePrice,
+//   checkedList,
+//   setCheckedList,
+// }) => {
+//   const [list, setList] = useState([]);
+
+//   const fetchFacilities = async () => {
+//     try {
+//       const { data } = await axios.get(`/api/facilities`);
+//       if (data?.facilities) {
+//         setList(data.facilities);
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   const handleCheckList = async (e) => {
+//     let newList = [];
+//     if (e.target.checked) {
+//       newList.push(e.target.value);
+//       setCheckedList(newList);
+//       return;
+//     }
+//     newList = newList.filter((i) => i !== e.target.value);
+//     setCheckedList(newList);
+//   };
+
+//   useEffect(() => {
+//     fetchFacilities();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className=" border-2 border-red-500 rounded-md m-5 h-auto py-10 px-3">
+//         <label htmlFor="price" className=" text-xl mr-3 font-bold">
+//           Price :{" "}
+//         </label>
+//         <input
+//           type="range"
+//           name="price"
+//           id="price"
+//           min={1000}
+//           max={3500}
+//           onChange={(e) => setPrice(e.target.value)}
+//           defaultValue={price ? price : 0}
+//         />
+//         <span className=" ml-10">&#8377; {price ? price : ""}</span>
+//         <div>
+//           <button
+//             className=" w-40 h-10 bg-green-300 cursor-pointer my-3"
+//             onClick={handlePrice}
+//           >
+//             Search
+//           </button>
+//         </div>
+//         <div className=" my-10 ">
+//           <h3 className=" text-xl font-bold my-3">Filter by Facilities : </h3>
+//           {list?.map((e) => {
+//             return (
+//               <p key={e} className="grid grid-cols-4 my-3">
+//                 <label htmlFor="checkbox" className=" col-span-2">
+//                   {e}{" "}
+//                 </label>
+//                 <input
+//                   type="checkbox"
+//                   name="ckeckbox"
+//                   id="checkbox"
+//                   value={e}
+//                   className=" w-5 h-5 ml-3 col-span-1"
+//                   onChange={handleCheckList}
+//                 />
+//               </p>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Filters;
+
+
+
+
 "use client";
 
 import axios from "axios";
@@ -39,10 +133,11 @@ const Filters = ({
   }, []);
 
   return (
-    <>
-      <div className=" border-2 border-red-500 rounded-md m-5 h-auto py-10 px-3">
-        <label htmlFor="price" className=" text-xl mr-3 font-bold">
-          Price :{" "}
+    <div className="border-2 border-red-500 rounded-md p-5 space-y-5">
+      {/* Price Range */}
+      <div>
+        <label htmlFor="price" className="text-xl font-bold">
+          Price:
         </label>
         <input
           type="range"
@@ -52,38 +147,41 @@ const Filters = ({
           max={3500}
           onChange={(e) => setPrice(e.target.value)}
           defaultValue={price ? price : 0}
+          className="w-full"
         />
-        <span className=" ml-10">&#8377; {price ? price : ""}</span>
-        <div>
-          <button
-            className=" w-40 h-10 bg-green-300 cursor-pointer my-3"
-            onClick={handlePrice}
-          >
-            Search
-          </button>
-        </div>
-        <div className=" my-10 ">
-          <h3 className=" text-xl font-bold my-3">Filter by Facilities : </h3>
-          {list?.map((e) => {
-            return (
-              <p key={e} className="grid grid-cols-4 my-3">
-                <label htmlFor="checkbox" className=" col-span-2">
-                  {e}{" "}
-                </label>
-                <input
-                  type="checkbox"
-                  name="ckeckbox"
-                  id="checkbox"
-                  value={e}
-                  className=" w-5 h-5 ml-3 col-span-1"
-                  onChange={handleCheckList}
-                />
-              </p>
-            );
-          })}
-        </div>
+        <span className="ml-5">&#8377; {price ? price : ""}</span>
       </div>
-    </>
+
+      {/* Search Button */}
+      <div className="flex justify-center">
+        <button
+          className="w-full md:w-40 h-10 bg-green-300 cursor-pointer"
+          onClick={handlePrice}
+        >
+          Search
+        </button>
+      </div>
+
+      {/* Filter by Facilities */}
+      <div>
+        <h3 className="text-xl font-bold">Filter by Facilities:</h3>
+        {list?.map((e) => (
+          <div key={e} className="flex items-center mb-3">
+            <label htmlFor="checkbox" className="text-lg flex-1">
+              {e}
+            </label>
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              value={e}
+              className="w-5 h-5"
+              onChange={handleCheckList}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
--- components/Hotel.jsx
+++ components/Hotel.jsx
@@ -1,78 +1,155 @@
+// import Image from "next/image";
+// import Link from "next/link";
+
+// const Hotel = ({ e }) => {
+//   return (
+//     <div className="border-2 border-red-500 rounded-lg h-96 w-full mb-5 p-5">
+//       <div className="flex">
+//         {/* Main Banner Image */}
+//         <Image
+//           src={e?.banner}
+//           alt={`Banner of ${e?.name}`}
+//           width={200}
+//           height={200}
+//           className="w-96 h-60 mr-3"
+//         />
+
+//         {/* Gallery Images */}
+//         <div className="flex flex-col justify-between">
+//           {e
+//             ? e.gallery?.map((ele, index) => {
+//                 return (
+//                   <Image
+//                     key={ele}
+//                     src={ele}
+//                     alt={`Gallery image ${index + 1} of ${e?.name}`}
+//                     width={200}
+//                     height={200}
+//                     className="w-28 h-16 object-cover"
+//                   />
+//                 );
+//               })
+//             : ""}
+//         </div>
+
+//         {/* Hotel Details */}
+//         <div className="ml-20">
+//           <h2 className="font-bold text-3xl line-clamp-1">{e?.name}</h2>
+//           <p className="text-justify my-5 text-lg">{e?.description}</p>
+
+//           {/* Facilities */}
+//           <div className="text-2xl my-5">
+//             <span className="font-bold">Facilities: </span>
+//             <ul className="flex">
+//               {e
+//                 ? e.facilities?.map((ele, index) => {
+//                     return (
+//                       <li
+//                         key={ele.name}
+//                         className="mr-10 mb-3 flex items-center"
+//                       >
+//                         <span>
+//                           <Image
+//                             src={ele.img}
+//                             alt={`Facility ${index + 1}: ${ele.name}`}
+//                             width={200}
+//                             height={200}
+//                             className="w-8 h-8 rounded-full"
+//                           />
+//                         </span>
+//                         <span className="ml-5">{ele.name}</span>
+//                       </li>
+//                     );
+//                   })
+//                 : ""}
+//             </ul>
+//           </div>
+
+//           {/* Price and Details Link */}
+//           <div className="flex items-center">
+//             <button className="w-60 h-14 rounded-lg bg-blue-400 text-lg">
+//               Price: &#8377; {e?.price}
+//             </button>
+//             <Link
+//               href={`/hotels/${e?._id}`}
+//               className="text-xl font-bold text-red-600 ml-10"
+//             >
+//               See Details
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hotel;
+
+
+
+
 import Image from "next/image";
 import Link from "next/link";
 
 const Hotel = ({ e }) => {
   return (
-    <div className="border-2 border-red-500 rounded-lg h-96 w-full mb-5 p-5">
-      <div className="flex">
+    <div className="border-2 border-red-500 rounded-lg h-auto md:h-96 w-full mb-5 p-5">
+      <div className="flex flex-col md:flex-row">
         {/* Main Banner Image */}
         <Image
           src={e?.banner}
           alt={`Banner of ${e?.name}`}
           width={200}
           height={200}
-          className="w-96 h-60 mr-3"
+          className="w-full md:w-96 h-60 object-cover mb-4 md:mb-0"
         />
 
         {/* Gallery Images */}
-        <div className="flex flex-col justify-between">
-          {e
-            ? e.gallery?.map((ele, index) => {
-                return (
-                  <Image
-                    key={ele}
-                    src={ele}
-                    alt={`Gallery image ${index + 1} of ${e?.name}`}
-                    width={200}
-                    height={200}
-                    className="w-28 h-16 object-cover"
-                  />
-                );
-              })
-            : ""}
+        <div className="flex flex-wrap justify-center md:justify-start space-x-3 md:space-x-6 mb-4 md:mb-0">
+          {e?.gallery?.map((ele, index) => (
+            <Image
+              key={ele}
+              src={ele}
+              alt={`Gallery image ${index + 1} of ${e?.name}`}
+              width={150}
+              height={100}
+              className="w-24 h-16 object-cover rounded-lg"
+            />
+          ))}
         </div>
 
         {/* Hotel Details */}
-        <div className="ml-20">
-          <h2 className="font-bold text-3xl line-clamp-1">{e?.name}</h2>
-          <p className="text-justify my-5 text-lg">{e?.description}</p>
+        <div className="ml-0 md:ml-5 mt-4 md:mt-0">
+          <h2 className="font-bold text-2xl md:text-3xl line-clamp-2">{e?.name}</h2>
+          <p className="text-justify my-3 text-lg">{e?.description}</p>
 
           {/* Facilities */}
-          <div className="text-2xl my-5">
+          <div className="text-2xl my-4">
             <span className="font-bold">Facilities: </span>
-            <ul className="flex">
-              {e
-                ? e.facilities?.map((ele, index) => {
-                    return (
-                      <li
-                        key={ele.name}
-                        className="mr-10 mb-3 flex items-center"
-                      >
-                        <span>
-                          <Image
-                            src={ele.img}
-                            alt={`Facility ${index + 1}: ${ele.name}`}
-                            width={200}
-                            height={200}
-                            className="w-8 h-8 rounded-full"
-                          />
-                        </span>
-                        <span className="ml-5">{ele.name}</span>
-                      </li>
-                    );
-                  })
-                : ""}
+            <ul className="flex flex-wrap space-x-6">
+              {e?.facilities?.map((ele, index) => (
+                <li key={ele.name} className="flex items-center mb-3">
+                  <Image
+                    src={ele.img}
+                    alt={`Facility ${index + 1}: ${ele.name}`}
+                    width={30}
+                    height={30}
+                    className="w-8 h-8 rounded-full"
+                  />
+                  <span className="ml-3">{ele.name}</span>
+                </li>
+              ))}
             </ul>
           </div>
 
           {/* Price and Details Link */}
-          <div className="flex items-center">
-            <button className="w-60 h-14 rounded-lg bg-blue-400 text-lg">
+          <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-10">
+            <button className="w-full md:w-60 h-14 rounded-lg bg-blue-400 text-lg">
               Price: &#8377; {e?.price}
             </button>
             <Link
               href={`/hotels/${e?._id}`}
-              className="text-xl font-bold text-red-600 ml-10"
+              className="text-xl font-bold text-red-600"
             >
               See Details
             </Link>
